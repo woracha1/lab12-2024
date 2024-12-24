9d1253d5463e4e54de3f62f225a1fcf34ba6b815
GGTTGTGt <zz814170@gmail.com>
2024-12-24 15:50:49 +0700

    lab12_1 completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,22 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int) ;
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int num){
+    if(num > 1){
+        return fibonacci(num-1) + fibonacci(num-2) ;
+    }else if(num == 1){
+       return 1 ;
+    }else{
+        return  0;
+    }
+    return 0 ;
 }
