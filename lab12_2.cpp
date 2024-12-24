8051b81ef1ac237ffa1f910d031048af7f168c3f
GGTTGTGt <zz814170@gmail.com>
2024-12-24 15:52:54 +0700

    lab12_2 completed

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int,int);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+int gcd(int x,int y){
+    if(x % y != 0){
+        return gcd(y, x%y) ;
+    }else{
+        return y ;
+    }
+    return 0 ;
 }
