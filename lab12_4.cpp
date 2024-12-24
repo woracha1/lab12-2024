f4a96ff7b77032e806f9001d11cd3c89d93ef06d
GGTTGTGt <zz814170@gmail.com>
2024-12-24 16:03:07 +0700

    lab12_4 completed

--- lab12_4.cpp
+++ lab12_4.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+template <typename I>
+void mySwap(I&,I&) ;
+
 int main(){
 	int x, y;
 	string a, b;
@@ -30,3 +33,9 @@ int main(){
 	return 0;
 }
 
+template <typename U>
+void mySwap(U &x,U &y){
+	U n = x ;
+	x = y ;
+	y = n ;
+}
